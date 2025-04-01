7b29bfb9bbbf85b784a901db7bda67f136e6b40b
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-01 20:57:49 +0000

    yuyen

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,28 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.println(sumOfDigits(123));
+		System.out.println(factorial(5));
+	}
 
+	public static int sumOfDigits(int x) {
+		if (x < 0) {
+			x = -x;
+		}
+		if (x < 10) {
+			return x;
+		}
+		return x % 10 + sumOfDigits(x / 10);
+	}
+
+	public static int factorial(int y){
+		if (y == 0) {
+			return 1;
+		}
+		if (y < 0){
+			return 0;
+		}
+		return y * factorial(y-1);
 	}
 
 	
